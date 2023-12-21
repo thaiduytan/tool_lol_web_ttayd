95c483e258767a59be184bb342f60e2abb535576
thaiduytan <102050031+thaiduytan@users.noreply.github.com>
2023-12-21 23:01:27 +0700

    21/12

--- components/Comparision/app.comparision.tsx
+++ components/Comparision/app.comparision.tsx
@@ -2,51 +2,39 @@ import { Box, Flex, Grid, GridCol, Image, Text, rem } from '@mantine/core';
 import React from 'react';
 import { MainServiceItem } from '../Service/app.service';
 import './app.comparision.scss';
+import { useMantineTheme } from '@mantine/core';
 // Interface cho props của component con
 interface ComparisionProps {
   data: MainServiceItem;
 }
 
 const Comparision: React.FC<ComparisionProps> = ({ data }) => {
-  const blurValue = '100px';
+  const theme = useMantineTheme();
   return (
     <>
-      <Box
-        w={rem('557px')}
-        pt={rem('74px')}
-        px={rem('43px')}
-        pb={rem('56px')}
-        className="comparision"
-        bg={data.bg}
-        style={{
-          borderRadius: '10px',
-          border: `${data.id === 2 ? '2px solid #5D8FDA' : ''}`,
-          boxShadow: `${data.id === 2 ? '0px 0px 4px rgba(110, 195, 253, 0.30)' : ''}`,
-        }}
-      >
-        <Flex display={'flex'} direction="column" gap={10} align="center">
-          <Text className="comparision-title">{data.title}</Text>
-          <Text className="comparision-desc">{data.desc}</Text>
-        </Flex>
-        <Box
-          className="comparision-service"
-          bg={data.bgListService}
-          style={{
-            borderRadius: `${data.id === 2 ? rem('389px') : ''}`,
-            // filter: `${data.id === 2 ? `blur(${blurValue})` : ''}`,
-          }}
+      <Box w={rem('557px')} pt={rem('74px')} px={rem('43px')} pb={rem('56px')} bg={data.bg}>
+        <Flex
+          display={'flex'}
+          direction="column"
+          gap={10}
+          align="center"
+          ff={"'Outfit', sans-serif"}
+          fs={'normal'}
+          style={{ color: theme.colors.black[0] }}
         >
+          <Text>{data.title}</Text>
+          <Text>{data.desc}</Text>
+        </Flex>
+        <Box>
           {data &&
             data?.listService.map((item) => {
               return (
-                <Grid key={`abc-${item.id}`} className="list-service" align="center">
-                  <GridCol span={1} className="list-service_icon-left">
+                <Grid key={`abc-${item.id}`} align="center">
+                  <GridCol span={1}>
                     <Image src={`/asset/img/${item.iconLeft}`}></Image>
                   </GridCol>
-                  <GridCol span={10} className="list-service_title">
-                    {item.titleService}
-                  </GridCol>
-                  <GridCol span={1} className="list_service_icon-right">
+                  <GridCol span={10}>{item.titleService}</GridCol>
+                  <GridCol span={1}>
                     <Image src={`/asset/img/${item.iconRight}`}></Image>
                   </GridCol>
                 </Grid>
--- theme.ts
+++ theme.ts
@@ -1,7 +1,23 @@
 'use client';
 
-import { createTheme } from '@mantine/core';
+import { createTheme, MantineProvider, rem } from '@mantine/core';
 
 export const theme = createTheme({
   /* Put your mantine theme override here */
+  colors: {
+    // Add your color
+    black: ['#rgba(30, 30, 30, 0.67)', '#C1CCF6'],
+  },
+
+  shadows: {
+    md: '1px 1px 3px rgba(0, 0, 0, .25)',
+    xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+  },
+
+  headings: {
+    fontFamily: 'Roboto, sans-serif',
+    sizes: {
+      h1: { fontSize: rem(36) },
+    },
+  },
 });
